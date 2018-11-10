9586ffbd31497d48cedea0eab99dc48acb722d5a
clickkiddie <clickkiddie@github.com>
2018-11-10 22:02:12 +0100

    adapted to drunkenSSH-logs

--- failauth.sh
+++ failauth.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 
 case "$1" in
-	"usernames")
-		cat /var/log/auth.log | grep -o "Failed password for .* from" | rev |awk '{print $2}' | rev | sort -u
-		;;
-	"cracker")
-		cat /var/log/auth.log | grep "Failed password" | grep -Po "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" | sort | uniq
-		;;
+	"attempts")
+		cat /var/log/auth.log | awk -F ":" '{print $4}' | grep "Failed password" | awk '{for(i=3; i < NF-2;i++){printf $i " "}print ""}' | awk -F " " '{print $(NF-2) " " $1 " " $NF}'
+	;;
 	"scanner")
 		cat /var/log/auth.log | grep "[preauth]"       | grep -Po "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" | sort | uniq
-	;;
-	"all")
-		cat /var/log/auth.log |                          grep -Po "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" | sort | uniq
 	;;
 	*)
 		echo "shows list of failed sshd login attempts"
 		echo "	usage: $0 {usernames|cracker|scanner|all}"
-		echo "		usernames: show list of usernames with failed login attempt"
-		echo "		cracker: show list of IP4 with failed login attempt"
+		echo "		attempts (requires drunkenSSH as sshd): show list of <user> <password> <ip> with failed login attempt"
 		echo "		scanner: show list of IP4 with disconnect before login"
-		echo "		all: show list of all IP4 with failed login attempt"
 	;;
 esac
